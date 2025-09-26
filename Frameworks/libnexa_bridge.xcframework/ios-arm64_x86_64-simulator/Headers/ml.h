3068a25d322f0ceeb1de8a2157155a9ce68ba037
xianda.yang <xianda.yang@nexa4ai.com>
2025-09-26 14:37:31 +0800

    update nexa_bridge.xcframework

--- Frameworks/libnexa_bridge.xcframework/ios-arm64_x86_64-simulator/Headers/ml.h
+++ Frameworks/libnexa_bridge.xcframework/ios-arm64_x86_64-simulator/Headers/ml.h
@@ -1349,3 +1349,25 @@ ML_API int32_t ml_cv_infer(const ml_CV* handle, const ml_CVInferInput* input, ml
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
+
+namespace nexa {
+class Plugin;
+}  // namespace nexa
+
+#if defined(_WIN32)
+#define PLUGIN_API __declspec(dllexport)
+#else
+#define PLUGIN_API __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+PLUGIN_API ml_PluginId plugin_id();
+PLUGIN_API nexa::Plugin* create_plugin();
+
+#ifdef __cplusplus
+}
+#endif
+
